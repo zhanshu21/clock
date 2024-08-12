c2f313510d0c8d412d8b1e88304d53864e3c66f8
Zhan Shu <zhan.shu@rwth-aachen.de>
2024-08-12 23:44:45 +0200

    initial commit: add all plain content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [breakTime, setBreakTime] = useState(5);
+  const [session, setSession] = useState(25);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="control-panel" className='control-panel'>
+        <div id="break-setter">
+          <h2 id='break-label' className='break-label'>Break Length</h2>
+          <button id='break-decrement' className='break-decrement'>decrement</button>
+          <div id="break-length">{breakTime}</div>
+          <button id='break-increment' className='break-increment'>increment</button>
+        </div>
+        <div id="session-setter">
+          <h2 id='session-label' className='session-label'>Session Length</h2>
+          <button id='session-decrement' className='session-decrement'>decrement</button>
+          <div id="session-length">{session}</div>
+          <button id='session-increment' className='session-increment'>increment</button>
+        </div>
+      </div>
+      <div id="timer" className="timer">
+        <h1 id="timer-label" className="timer-label">Session</h1>
+        <div id="time-left" className="time-left">{session}</div>
+        <button id="start_stop" className="start_stop">start/stop</button>
+        <button id="reset" className="reset">reset</button>
+      </div>
     </div>
   );
 }
